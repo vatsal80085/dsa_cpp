2591c9d2905e97f3040b0653065bc73419777663
Vatsal Agarwal <vatsal08agarwal@gmail.com>
2024-07-22 18:49:47 +0000

    pattern printing problems

---
+++ day3.cpp
@@ -0,0 +1,235 @@
+// #include<iostream>
+// using namespace std;
+// int main()
+
+// {
+//     // PATTERNS
+//     // 1
+//     // int n;
+//     // cin>>n;
+//     // int i=1;
+//     // while(i<=n){
+//     //     int j=1;
+//     //     while(j<=n){
+//     //         cout<< "*";
+//     //         j=j+1;
+//     //     }
+//     //     cout<<endl;
+//     //     i=i+1;
+//     //}
+
+//     //2
+//     // int n;
+//     // cin >> n;
+//     // int i = 1;
+//     // while (i <= n)
+//     // {
+//     //     int j = 1;
+//     //     while (j <= n)
+//     //     {
+//     //         cout << i;
+//     //         j = j + 1;
+//     //     }
+//     //     cout << endl;
+//     //     i = i + 1;
+//     // }
+
+//     //3
+//     // int n;
+//     // cin>>n;
+//     // int i=1;
+//     // while(i<=n){
+//     //     int j=1;
+//     //     while(j<=n){
+//     //         cout<<j;
+//     //         j++;
+
+//     //     }
+//     //     cout<<endl;
+//     //     i++;
+//     // }
+
+//     //4
+//     // int n;
+//     // cin>>n;
+//     // int i=1;
+//     // while(i<=n){
+//     //     int j=1;
+//     //     while(j<=n){
+//     //         cout<<n-j+1;
+//     //         j++;
+//     //     }
+//     //     cout<<endl;
+//     //     i++;
+//     // }
+
+//     //5
+//     // int n;
+//     // cin>>n;
+//     // int i=1,count=1;
+//     // while(i<=n){
+//     //     int j=1;
+//     //     while(j<=n){
+//     //         cout<<count<<" ";
+//     //         count=count+1;
+//     //         j=j+1;
+
+//     //     }
+//     //     cout<<endl;
+//     //     i++;
+//     // }
+
+//     // int n;
+//     // cin>>n;
+//     // int row=1;
+//     // while(row<=n){
+//     //     int colm=1;
+//     //     while(colm<=row){
+//     //         cout<<"*";
+//     //         colm++;
+//     //     }
+//     //     cout<<endl;
+//     //     row++;
+
+//     // }
+//     // cin>>n;
+//     // row=1;
+//     // while(row<=n){
+//     //     int colm=1;
+//     //     while(colm<=row){
+//     //         cout<<row;
+//     //         colm++;
+
+//     //     }
+//     //     cout<<endl;
+//     //     row++;
+//     // }
+//     // cin>>n;
+//     // row=1;
+//     // while(row<=n){
+//     //     int colm=1;
+//     //     while(colm<=row){
+//     //         cout<<colm;
+//     //         colm++;
+
+//     //     }
+//     //     cout<<endl;
+//     //     row++;
+//     // }
+
+//     return 0;
+// }
+
+// #include<iostream>
+// using namespace std;
+// int main()
+// {
+    //patterns continued 
+    // int n;
+    // cin>>n;
+    // int i =1;
+    // while(i<=n){
+    //     int j=1;
+    //     while(j<=n){
+    //         cout<<j<<" ";
+    //         j=j+1;
+    //     }
+    //     cout<<endl;
+    //     i=i+1;
+    // }
+
+    // int n;
+    // cin>>n;
+    // int i =1;
+    // while(i<=n){
+    //     int j=1;
+    //     while(j<=n){
+    //         cout<<n-j+1<<" ";
+    //         j=j+1;
+
+    //     }
+    //     cout<<endl;
+    //     i=i+1;
+    // }
+
+//     int n;
+//     cin>>n;
+//     int i=i+1;
+//     int count=1;
+//     while(i<=n){
+//         int j=1;
+//         while(j<=n){
+//             cout<<count<<" ";
+//             count=count+1;
+//             j=j+1;
+//         }
+//         cout<<endl;
+//         i=i+1;
+//
+    //int n;
+    // cin>>n;
+    // int row=1;
+    // while(row<=n){
+    //     int colm=1;
+    //     while(colm<=row){
+    //         cout<<colm<<" ";
+    //         colm=colm+1;
+    //     }
+    //     cout<<endl;
+    //     row=row+1;
+    // }
+    // cin>>n;
+    // int row=1;
+    // while(row<=n){
+    //     int colm=1;
+    //     while(colm<=row){
+    //         cout<<n-colm+1<<" ";
+    //         colm=colm+1;
+    //     }
+    //     cout<<endl;
+    //     row=row+1;
+    // }
+
+    // cin>>n;
+    // int i=1;
+    // int count=1;
+    // while(i<=n){
+    //     int j=1;
+    //     while(j<=i){
+    //         cout<<count<<" ";
+    //         count+=1;
+    //         j=j+1;
+    //     }
+    //     cout<<endl;
+    //     i=i+1;
+    // }
+    // cin>>n;
+    // int i=1;
+    // while(i<=n){
+    //     int j=1;
+    //     int value=i;
+    //     while(j<=i){
+    //         cout<<value<<" ";
+    //         value+=1;
+    //         j+=1;
+    //     }
+    //     cout<<endl;
+    //     i+=1;
+    // }
+
+//     cin>>n;
+//     int i=1;
+//     while(i<=n){
+//         int j=1;
+        
+//         while(j<=i){
+            
+//             cout<<i-j+1<<" ";
+//             j+=1;
+//         }
+//         cout<<endl;
+//         i+=1;
+//     }
+
+// }
+
