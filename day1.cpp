4773aa6d892c0ce827a0e35c24fc2f0981ab7d14
Vatsal Agarwal <vatsal08agarwal@gmail.com>
2024-07-22 18:45:38 +0000

    variables and datatypes

---
+++ day1.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // cout << "I am Vatsal";
+
+    // int a = 123;
+    // cout << a << endl;
+    // char b = 'v';
+    // cout << b << endl;
+    bool bl = true;
+    cout << bl << endl;
+    // float f = 1.234;
+    // cout << f << endl;
+
+    // int size = sizeof(a);
+    // cout << "Size of a is: "<< size << endl;
+
+    int a = 'a';
+    cout << a << endl;
+    char ch = 98;
+    cout << ch << endl;
+}
